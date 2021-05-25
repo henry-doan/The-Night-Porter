6fd3d285da4ef5e058db287fb3db2ea24edde2eb
Henry Doan <henrydoan96@gmail.com>
2021-05-25 17:27:02 -0600

    notes and change txt

--- The Night Porter/ViewController.swift
+++ The Night Porter/ViewController.swift
@@ -4,7 +4,9 @@
 //
 //  Created by Henry Doan on 5/13/21.
 //
-
+// link all the ui stuff appl provided to do ios apps, not just ui but infrastrure too
+// sub view, a view obj inside another one
+// none of the items doesn't have a unique id
 import UIKit
 
 class ViewController: UIViewController {
@@ -16,6 +18,18 @@ class ViewController: UIViewController {
     
     @IBAction func changeBg(_ sender: Any) {
         view.backgroundColor = UIColor.darkGray
+        // get all elements that are in the top view object
+        let everything = view.subviews
+        
+        for eachView in everything {
+            // is a label
+            if eachView is UILabel {
+                // downcast as uilabel
+                // ! force it to down cast or else it will give us errs
+                let currLbl = eachView as! UILabel
+                currLbl.textColor = UIColor.lightGray
+            }
+        }
     }
     
 
