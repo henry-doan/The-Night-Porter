5b45a0ee32a13f5c3c33bb5450447b3193beb125
Henry Doan <henrydoan96@gmail.com>
2021-06-10 16:09:55 -0600

    refactor to table view

--- The Night Porter/ViewController.swift
+++ The Night Porter/ViewController.swift
@@ -10,8 +10,72 @@
 // when draging controls, it doesn't generate swift or any code its just xml
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDataSource {
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 3
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        switch section {
+        case 0:
+            return dailyTasks.count
+        case 1:
+            return weeklyTasks.count
+        case 2:
+            return monthlyTasks.count
+        default:
+            return 0
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        
+        switch indexPath.section {
+        case 0:
+            cell.textLabel?.text = dailyTasks[indexPath.row]
+        case 1:
+            cell.textLabel?.text = weeklyTasks[indexPath.row]
+        case 2:
+            cell.textLabel?.text = monthlyTasks[indexPath.row]
+        default:
+            cell.textLabel?.text = "This shouldn't happen"
+        }
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        switch section {
+        case 0:
+            return "Daily Tasks"
+        case 1:
+            return "Weekly Tasks"
+        case 2:
+            return "Montly Tasks"
+        default:
+            return nil
+        }
+    }
+    
+    
+    // create string arr of tasks
+    let dailyTasks = ["Check all windows",
+                      "Check all doors",
+                      "Is the boiler fueled?",
+                      "Check mailbox",
+                      "Empty trash",
+                      "If freezing, check water pipes",
+                      "Document \"strange and unusual\" events",]
+    
+    let weeklyTasks = ["Check inside all cabins",
+                       "Flush all toilets",
+                       "Walk perimeter",]
+    
+    let monthlyTasks = ["Test security alarm",
+                        "Test motion detectors",
+                        "Test smoke alarms"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
